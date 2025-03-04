c28078c5b003506f5f3e01792294119768c68647
gkibria <gkibria121@gmail.com>
2025-03-04 11:26:34 +0600

    wip

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -5,7 +5,9 @@ const MONGO_URI = process.env.MONGO_URI as string;
 export async function connectDB() {
   if (mongoose.connection.readyState >= 1) return;
   try {
-    await mongoose.connect(MONGO_URI);
+    await mongoose.connect(MONGO_URI, {
+      serverSelectionTimeoutMS: 30000, // 30 seconds timeout
+    });
     console.log("✅ MongoDB Connected");
   } catch (err) {
     console.error("❌ MongoDB Connection Error :", err);
